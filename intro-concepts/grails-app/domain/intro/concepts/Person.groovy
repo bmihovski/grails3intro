b3a2ea7c2c17980c63ac59884c04e681f7862adb
bmihovski <bmihovski@live.com>
2019-11-15 23:01:36 +0200

    Domain class and validation

--- intro-concepts/grails-app/domain/intro/concepts/Person.groovy
+++ intro-concepts/grails-app/domain/intro/concepts/Person.groovy
@@ -5,4 +5,13 @@ class Person {
     String firstName
     String lastName
     Boolean active = false
+    String emailAddress
+
+    static constraints = {
+        emailAddress nullable: false, email: true, unique: true
+    }
+
+    String toString() {
+        firstName + " " + lastName
+    }
 }
--- intro-concepts/grails-app/init/intro/concepts/BootStrap.groovy
+++ intro-concepts/grails-app/init/intro/concepts/BootStrap.groovy
@@ -3,6 +3,13 @@ package intro.concepts
 class BootStrap {
 
     def init = { servletContext ->
+        def person = new Person (firstName: "Eric",
+                lastName: "Helgenson")
+        person.firstName = "Bob"
+        assert !person.active
+        person.save(flush: true)
+        Person.findByFirstName("Bob")
+
     }
     def destroy = {
     }
