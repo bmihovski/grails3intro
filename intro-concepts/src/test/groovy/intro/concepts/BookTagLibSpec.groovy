418fa7e21edef6c3a7516312edd1674f46f4cd88
bmihovski <bmihovski@live.com>
2019-11-19 10:57:34 +0200

    Taglibs tests fixed

--- intro-concepts/src/test/groovy/intro/concepts/BookTagLibSpec.groovy
+++ intro-concepts/src/test/groovy/intro/concepts/BookTagLibSpec.groovy
@@ -6,6 +6,8 @@ import spock.lang.Specification
 class BookTagLibSpec extends Specification implements TagLibUnitTest<BookTagLib> {
 
     def setup() {
+        // Ensure en_US is set for locale for currency format
+        request.addPreferredLocale(Locale.US)
     }
 
     def cleanup() {
@@ -13,10 +15,10 @@ class BookTagLibSpec extends Specification implements TagLibUnitTest<BookTagLib>
 
     void "test price"() {
         expect:
-            tagLib.price(price: 1_00).toString() == "<div class='price' data-price='100'/>\$1.00</div>"
-            tagLib.price(price: 1_10).toString() == "<div class='price' data-price='110'/>\$1.10</div>"
-            tagLib.price(price: 1_11).toString() == "<div class='price' data-price='111'/>\$1.11</div>"
-            tagLib.price(price: 1_000_10).toString() == "<div class='price' data-price='100010'/>\$1,000.10</div>"
+            tagLib.price(price: 1_00).toString() == "<div class='price' data-price='100'>\$1.00</div>"
+            tagLib.price(price: 1_10).toString() == "<div class='price' data-price='110'>\$1.10</div>"
+            tagLib.price(price: 1_11).toString() == "<div class='price' data-price='111'>\$1.11</div>"
+            tagLib.price(price: 1_000_10).toString() == "<div class='price' data-price='100010'>\$1,000.10</div>"
     }
 
     void "test display"() {
@@ -24,10 +26,9 @@ class BookTagLibSpec extends Specification implements TagLibUnitTest<BookTagLib>
             def book = [id: 1, title: "foo", price: 1_00]
         expect:
             tagLib.display(book: book).toString() == """<div class='book'>
-            <h2>foo</h2>
-            <div class='price' data-price='100'>\$1.00</div>
-            <a href="/example/tagLibs/1">BUY</a>
-            </div>
-            """
+    <h2>foo</h2>
+    <div class='price' data-price='100'>\$1.00</div>
+    <a href="/example/tagLibs/1">BUY</a>
+</div>"""
     }
 }
